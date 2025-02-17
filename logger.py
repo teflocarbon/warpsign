38cc1f373dfb567785c545a84d92a9d622dfdd96
teflocarbon <github@teflo.cc>
2025-02-17 19:28:27 +1100

    Try get colour with GitHub Actions.

--- logger.py
+++ logger.py
@@ -5,4 +5,4 @@ from functools import lru_cache
 @lru_cache(maxsize=1)
 def get_console() -> Console:
     """Get or create the shared Console instance"""
-    return Console()
+    return Console(force_terminal=True, color_system="auto")
